2c27c057d911ea93c26cae029fc993d7f0b3135b
Kyle Stearns <kylestearns@cobra-hunter.home>
2020-03-23 19:22:03 -0400

    add total tomato count to history display

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -8,21 +8,23 @@ const History = ({ history }: { history: any }) => {
       {days.map((day: any) => {
         return (
           <div key={day.date}>
-            <h4>{day.title}</h4>
-            {day.items.length && (
-              <ul>
-                {day.items.map((pomodoro: any) => {
-                  return (
-                    <li key={pomodoro.timeInitiated}>
-                      <span
-                        style={{ color: '#7d7d7d' }}
-                      >{`${pomodoro.timeStartedString} - ${pomodoro.timeCompletedString}`}</span>{' '}
-                      {pomodoro.description}
-                    </li>
-                  );
-                })}
-              </ul>
-            )}
+            <h4>
+              {day.title}
+              <br />
+              {day.items.length} completed
+            </h4>
+            <ul>
+              {day.items.map((pomodoro: any) => {
+                return (
+                  <li key={pomodoro.timeInitiated}>
+                    <span
+                      style={{ color: '#7d7d7d' }}
+                    >{`${pomodoro.timeStartedString} - ${pomodoro.timeCompletedString}`}</span>{' '}
+                    {pomodoro.description}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         );
       })}
